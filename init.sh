cee98fea43c95b23768b2425f9abc0cb35ebdbbb
lassertos <50876686+lassertos@users.noreply.github.com>
2020-10-19 19:49:20 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,6 +4,6 @@ source oe-init-build-env
 bitbake-layers add-layer "../meta-rauc"
 bitbake-layers add-layer "../meta-goldi"
 bitbake-layers add-layer "../meta-raspberrypi"
-bitbake-layers add-layer "../meta-microcontroller"
 bitbake-layers add-layer "../meta-openembedded/meta-oe"
+bitbake-layers add-layer "../meta-microcontroller"
 bitbake-layers add-layer "../meta-openembedded/meta-python"
