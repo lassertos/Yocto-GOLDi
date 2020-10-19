3758b9ca7aac912d3f6b25cabb6c998829d4249e
lassertos <50876686+lassertos@users.noreply.github.com>
2020-10-19 19:47:34 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
+#check for sudo
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
+
 #init build environment
 source oe-init-build-env
 
