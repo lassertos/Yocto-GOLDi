e6fb66c0b20eac9e43dc7344f36b598388074a14
Pierre Helbing <pierre.helbing@tu-ilmenau.de>
2020-10-19 18:48:55 +0200

    updated build-scripts

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ MACHINE=raspberrypi3
 UPDATE_DEPLOY_DIR=/var/www/html/updates/
 
 #collect commitnumber and deploydir location
-#COMMITNR=$(git rev-parse HEAD)
-COMMITNR=$(bitbake -e | grep DATETIME= | cut -d= -f2 | sed 's/"//g')
+COMMITNR=$(git rev-parse HEAD)
+#COMMITNR=$(bitbake -e | grep DATETIME= | cut -d= -f2 | sed 's/"//g')
 temp="${COMMITNR%\"}"
 STARTTIME="${temp#\"}"
 DEPLOY_DIR_IMAGE=$(bitbake -e | grep DEPLOY_DIR_IMAGE= | cut -d= -f2)
@@ -20,5 +20,5 @@ export BB_ENV_EXTRAWHITE="$BB_ENV_EXTRAWHITE COMMITNR"
 bitbake core-image-minimal-goldi update-bundle
 
 #copy raucb file to deploydir and create update-info
-#ls $DEPLOY_DIR_IMAGE | grep update-bundle-${MACHINE}- | xargs -I '{}' cp $DEPLOY_DIR_IMAGE/{} ${UPDATE_DEPLOY_DIR}update.raucb
-#rauc info ${UPDATE_DEPLOY_DIR}update.raucb > ${UPDATE_DEPLOY_DIR}update-info
+ls $DEPLOY_DIR_IMAGE | grep update-bundle-${MACHINE}- | xargs -I '{}' cp $DEPLOY_DIR_IMAGE/{} ${UPDATE_DEPLOY_DIR}update.raucb
+rauc info ${UPDATE_DEPLOY_DIR}update.raucb > ${UPDATE_DEPLOY_DIR}update-info
