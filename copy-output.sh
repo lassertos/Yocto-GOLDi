15df4ddd21b34d2a7a6badf5096f1f59481ccaf2
Pierre Helbing <pierre.helbing@tu-ilmenau.de>
2020-11-28 16:46:04 +0100

    changed machine to rpi 4 for testing and partly reverted update mechanism

--- copy-output.sh
+++ copy-output.sh
@@ -7,10 +7,12 @@ while getopts "cp" opt; do
     	c)
       		cp $2/update-bundle-CU-$3.raucb ${UPDATE_DEPLOY_DIR}
       		cp $2/image-CU-$3.wic.bz2 ${UPDATE_DEPLOY_DIR}
+      		tmp/deploy/tools/rauc -c tmp/deploy/tools/system.conf info ${UPDATE_DEPLOY_DIR}update-bundle-CU-$3.raucb > ${UPDATE_DEPLOY_DIR}update-info-CU-$3
       		;;
     	p)
       		cp $2/update-bundle-PS-$3.raucb ${UPDATE_DEPLOY_DIR}
       		cp $2/image-PS-$3.wic.bz2 ${UPDATE_DEPLOY_DIR}
+      		tmp/deploy/tools/rauc -c tmp/deploy/tools/system.conf info ${UPDATE_DEPLOY_DIR}update-bundle-PS-$3.raucb > ${UPDATE_DEPLOY_DIR}update-info-PS-$3
       		;;
     	\?)
       		echo "Invalid option: -$OPTARG" >&2
